05ecc1873884ec607b3275bd5e2097366e0ca921
KaiEngelmann <KaiVaughnMusic@Yahoo.com>
2025-07-21 16:52:50 -0400

    added file path checking

---
+++ get_files_info.py
@@ -0,0 +1,35 @@
+import os
+
+def get_files_info(working_directory, directory="."):
+    working_directory = os.path.abspath(working_directory)
+    absolute_path = os.path.abspath(os.path.join(working_directory, directory))
+    if os.path.commonpath([absolute_path, working_directory]) != working_directory:
+        return (f"""Error: Cannot list "{directory}" as it is outside the permitted working directory""")
+    if not os.path.isdir(absolute_path):
+        return (f"""Error: "{directory}" is not a directory""")
+    contents = os.listdir(absolute_path)
+    file_list = []
+    file_size = []
+    is_dir = []
+    for file in contents:
+        full_path = os.path.join(absolute_path, file)
+        try:
+            file_list.append(file)
+            file_size.append(os.path.getsize(full_path))
+            is_dir.append(os.path.isdir(full_path))
+        except OSError as e:
+            return f"Error: Could not access '{file}': {e}"
+    results = f"Result for {directory} directory:\n"
+    for i in range(len(file_list)):
+        name = file_list[i]
+        size = file_size[i]
+        is_directory = is_dir[i]
+        results += f" - {name}: File_size={size}, is_dir={is_directory}\n"
+    return results
+    
+
+
+
+
+
+    
